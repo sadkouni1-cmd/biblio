fbe11aaca7c4c88a93714b38448d29a74d40f39a
ebob3294-hub <h7roune@gmail.com>
2026-05-04 09:58:56 +0100

    intitial android and capacitor setup

---
+++ android/app/src/main/java/com/kutub/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.kutub.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kutub.app',
+  appName: 'kutub',
+  webDir: 'dist'
+};
+
+export default config;
